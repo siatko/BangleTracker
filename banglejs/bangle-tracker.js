b5eabe13bb51c865722e4cdbe18c5a1c38c92415
Simon Weis <simonattilaweis@tutanota.com>
2020-09-12 22:12:21 +0200

    Create bangle-tracker.js

---
+++ banglejs/bangle-tracker.js
@@ -0,0 +1,24 @@
+/* jshint esversion: 6 */
+/*
+ * BangleTracker - WatchApp
+ */
+
+{
+    // Screen
+    const screen = {
+        width:  g.getWidth(),
+        height: g.getHeight(),
+        top:    g.getHeight() / 3,
+        center: g.getHeight() / 2,
+        bottom: g.getHeight() / 1.25,
+    }
+
+    // Main colors
+    const colors = {
+        black:  '0x0000',
+        white:  '0xffff',
+        green:  '0x03e0',
+        orange: '0xfd20',
+        red:    '0x7800',
+    }
+}
