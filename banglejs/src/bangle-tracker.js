c9f927d73035941cd3da1be0b3f04e710132388d
Simon Weis <simonattilaweis@tutanota.com>
2020-09-21 20:54:51 +0200

    Update bangle app ui;

--- banglejs/src/bangle-tracker.js
+++ banglejs/src/bangle-tracker.js
@@ -19,22 +19,56 @@ require("Font7x11Numeric7Seg").add(Graphics);
 
 function drawTimer() {
   // Position
-  var posX = screen.hcenter + 107, posY = screen.vtop + 10;
-  // Time
+  var posX = screen.hcenter + 55, posY = screen.vtop + 20;
+  // Timer
   var hour = '00';
   var minute = '00';
   var second = '00';
   var time = (" " + hour).substr(-2) + ":" + ("0" + minute).substr(-2) + ":" + ("0" + second).substr(-2);
   g.reset();
-  g.setFont("7x11Numeric7Seg", 4);
+  g.setFont("7x11Numeric7Seg", 2);
   g.setFontAlign(1, 1);
   g.setColor(1, 1, 1);
   g.drawString(time, posX, posY, true);
 
 }
 
+function drawClock() {
+  // Position
+  var posX = screen.hcenter + 57, posY = screen.vtop + 90;
+  // Time
+  let d = Date();
+  var time = (" 0" + d.getHours()).substr(-2) + ":" + ("0" + d.getMinutes()).substr(-2);
+  g.reset();
+  g.setFont("7x11Numeric7Seg", 4);
+  g.setFontAlign(1, 1);
+  g.setColor(1, 0.65, 1);
+  g.drawString(time, posX, posY, true);
+  g.setFont("7x11Numeric7Seg", 2);
+  g.setFontAlign(1, 1);
+  g.setColor(1, 0.65, 1);
+  g.drawString(("0" + d.getSeconds()).substr(-2), posX +30, posY, true);
+
+}
+
+function drawDate() {
+  // Position
+  var posX = screen.hcenter + 57, posY = screen.vtop + 120;
+  // Time
+  let d = Date();
+  var day = d.getDate();
+  var month = d.getMonth();
+  var year = d.getFullYear();
+  var time = ("0" + day).substr(-2) + "." + ("0" + month).substr(-2) + "." + year;
+  g.reset();
+  g.setFont("7x11Numeric7Seg", 2);
+  g.setFontAlign(1, 1);
+  g.setColor(1, 1, 1);
+  g.drawString(time, posX, posY, true);
+}
+
 function drawBpm(bpmValue) {
-  var posX = screen.hcenter + 25, posY = screen.vcenter + 10;
+  var posX = 70, posY = screen.vbottom + 27;
   g.reset();
   g.setFont("7x11Numeric7Seg", 2);
   g.setFontAlign(1, 1);
@@ -43,7 +77,7 @@ function drawBpm(bpmValue) {
 }
 
 function drawSteps(stepValue) {
-  var posX = screen.hcenter + 35, posY = screen.vbottom - 10;
+  var posX =  220, posY = screen.vbottom + 27;
   g.reset();
   g.setFont("7x11Numeric7Seg", 2);
   g.setFontAlign(1, 1);
@@ -51,19 +85,13 @@ function drawSteps(stepValue) {
   g.drawString(stepValue, posX, posY, true);
 }
 
-function drawGpsStatus(connected) {
+function drawHrmStatus(connected) {
   if (connected) {
     g.setColor(0, 1, 0);
   } else {
     g.setColor(1, 0, 0);
   }
-  g.fillCircle(screen.hcenter, screen.vbottom + 15, 10);
-}
-
-function drawLines() {
-  g.setColor(0, 0, 1);
-  g.drawLine(0, 90, g.getWidth(), 90);
-  g.drawLine(0, 145, g.getWidth(), 145);
+  g.fillCircle(screen.hcenter - 85, screen.vcenter + 30, 7);
 }
 
 function getAvg(values) {
@@ -72,11 +100,12 @@ function getAvg(values) {
 }
 
 // Sensors
-Bangle.setHRMPower(1);
 
 // Draw
 g.clear();
 drawTimer();
+drawClock();
+drawDate();
 
 hrtArr = [];
 Bangle.on('HRM', function(hrm) {
@@ -85,7 +114,7 @@ Bangle.on('HRM', function(hrm) {
     wasMeasured = hrm.bpm;
   }
   if(hrtArr.length > 5) {
-    drawBpm('000');
+    drawBpm('     ');
     drawBpm(parseInt(getAvg(hrtArr)));
     NRF.updateServices({
       0x180D: { // heart_rate
@@ -107,11 +136,9 @@ Bangle.on('HRM', function(hrm) {
 setInterval(drawTimer, 1000);
 
 // First Draw Bpm and Steps
-drawBpm('000');
-drawSteps('00000');
-
-drawGpsStatus(true);
-drawLines();
+drawBpm('     ');
+drawSteps('     ');
+drawHrmStatus(false);
 
 
 Bangle.on('step', function (steps){
@@ -128,4 +155,20 @@ NRF.setServices({
   }
 }, { advertise: [ '180D' ] });
 
-// Intervals
+var started = false;
+setWatch(function() { // Reset
+  if(!started) {
+    Bangle.setHRMPower(1);
+    started = true;
+    drawHrmStatus(true);
+  }  else {
+    Bangle.setHRMPower(0);
+    started = false;
+    drawHrmStatus(false);
+    drawBpm('000')
+  }
+}, BTN2, {repeat:true});
+
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+setInterval(drawClock, 1000);
